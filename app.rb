4e6cc81565d0ff0f0ec0d03594893a52ea91853e
Yannick Schutz <yannick.schutz@gmail.com>
2013-11-17 17:32:07 +0100

    remove cache from separate file

--- app.rb
+++ app.rb
@@ -1,15 +1,28 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'dotenv'
-require 'cache'
 require 'newrelic_rpm'
 require 'github_repos'
+require 'dalli'
+require 'rack-cache'
 
 Dotenv.load
 
 
 class App < Sinatra::Base
-  include Cache
+  if ENV["MEMCACHEDCLOUD_SERVERS"]
+    cache = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"].split(","),
+                              {:username => ENV["MEMCACHEDCLOUD_USERNAME"],
+                               :password => ENV["MEMCACHEDCLOUD_PASSWORD"],
+                               :failover => true,
+                               :socket_timeout => 1.5,
+                               :socket_failure_delay => 0.2
+    })
+    use Rack::Cache,
+      verbose: true,
+      metastore:   cache,
+      entitystore: cache
+  end
 
   configure do
     set :show_exceptions, false
--- cache.rb
+++ cache.rb
@@ -1,17 +1,2 @@
-require 'dalli'
-require 'rack-cache'
 module Cache
-  if ENV["MEMCACHEDCLOUD_SERVERS"]
-    cache = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"].split(","),
-                              {:username => ENV["MEMCACHEDCLOUD_USERNAME"],
-                               :password => ENV["MEMCACHEDCLOUD_PASSWORD"],
-                               :failover => true,
-                               :socket_timeout => 1.5,
-                               :socket_failure_delay => 0.2
-    })
-    use Rack::Cache,
-      verbose: true,
-      metastore:   cache,
-      entitystore: cache
-  end
 end
